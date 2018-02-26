6a541ef1e9bae5f6fe31df8458169aa5b928a04d
Dinesh Pithiya <dinesh.pithiya@moontech.int>
2018-02-26 18:27:34 +0530

    dinesh pithiya

--- example-1.php
+++ example-1.php
@@ -1,13 +1,22 @@
-<?php 
-require('ss-ga.class.php');
-$ssga = new ssga( 'UA-114573654-1', 'analytic.domain.com' );
-//Set a pageview
-$ssga->set_page( '/example/index.php' );
-$ssga->set_page_title( 'Page Title' );
-// Send
+```<?php 
+```
+>> include ss-ga.class
 
-//Ecommerce tracking
-/*==set transaction property==*/
+```require('ss-ga.class.php');```
+>> Open this URL https://analytics.google.com/analytics/web/ and if you don't have account then create
+After create project, click left hand side on top menu icon google analytic suite. now create new project
+
+>> set track id and set your verified domain name
+
+```$ssga = new ssga( 'UA-114573654-1', 'analytic.domain.com' );```
+>> Set a pageview
+```$ssga->set_page( '/example/index.php' );
+$ssga->set_page_title( 'Page Title' );```
+>> Send
+
+>> Ecommerce tracking
+
+```/*==set transaction property==*/
 $TrackingId='UA-114573654-1';
 $URL="analytic.domain.com";
 $transaction_id=111111111111111;
@@ -33,5 +42,5 @@ $step2 = new ssga($TrackingId,$URL);
 $step2->send_item($transaction_id, $sku,$product_name,$variation,$unit_price,$quantity);
 /*==end==*/
 
-echo json_encode("suceess");
-?>
+echo json_encode("suceess");```
+?>```
